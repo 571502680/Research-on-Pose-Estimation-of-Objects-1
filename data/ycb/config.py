4de506136a204507c0798396e3ffc891f4814af0
LiQi <43883751+571502680@users.noreply.github.com>
2019-10-21 19:48:45 +0800

    Create config.py

---
+++ data/ycb/config.py
@@ -0,0 +1,5 @@
+LINEMOD = '/home/common/liqi/data/LINEMOD_6D/LM6d_origin/test'  # LINEMOD data root
+LINEMODNAMES = ('ape', 'bvise', 'bowl', 'camera', 'can', 'cat', 'cup',
+                'driller', 'duck', 'eggbo', 'glue', 'holepuncher', 'iron', 'lamp', 'phone')
+
+DARKNETDATA = '/home/common/liqi/data/LINEMOD_6D/LM6d_origin/data'  # Darknet data root
